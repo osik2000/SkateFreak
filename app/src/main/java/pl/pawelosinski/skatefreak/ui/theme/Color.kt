14336e5c4fa56245dcb9b471c815448d1164b275
Paweł Osiński <pawelosinski000@gmail.com>
2023-11-01 22:56:28 +0100

    Change basic color for one theme

--- app/src/main/java/pl/pawelosinski/skatefreak/ui/theme/Color.kt
+++ app/src/main/java/pl/pawelosinski/skatefreak/ui/theme/Color.kt
@@ -2,9 +2,7 @@ package pl.pawelosinski.skatefreak.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DarkThemeError = Color(0xFFFF0000)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
--- app/src/main/java/pl/pawelosinski/skatefreak/ui/theme/Theme.kt
+++ app/src/main/java/pl/pawelosinski/skatefreak/ui/theme/Theme.kt
@@ -10,31 +10,34 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkThemeError,
+    secondary = DarkThemeError,
+    tertiary = DarkThemeError,
+    background = Color(0xFF000000),
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = Color(0xFFE256BF),
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
+    /* Other default colors to override */
+
+    surface = Color(0xFFFF0000),
+    onPrimary = Color(0xFFFF0000),
+    onSecondary = Color(0xFFFF0000),
+    onTertiary = Color(0xFFFF0000),
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
+
 )
 
 @Composable
