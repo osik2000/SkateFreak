e8267bab05477d9e7431cca8d29c02c12e984769
Paweł Osiński <63377769+osik2000@users.noreply.github.com>
2023-11-25 19:54:21 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
+
+implementation("androidx.fragment:fragment-ktx:1.6.2")    
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
