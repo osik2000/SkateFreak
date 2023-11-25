85a507f97a829fe500267bbe1d31f62f5ed65ac8
Paweł Osiński <63377769+osik2000@users.noreply.github.com>
2023-11-25 19:22:47 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,9 @@ android {
 dependencies {
 
 implementation(platform("androidx.compose:compose-bom"))
-    androidTestImplementation(platform("androidx.compose:compose-bom"))    
+// Import the Compose BOM
+    implementation platform('androidx.compose:compose-bom:2023.10.01')
+   androidTestImplementation platform('androidx.compose:compose-bom:2023.10.01'))    
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
