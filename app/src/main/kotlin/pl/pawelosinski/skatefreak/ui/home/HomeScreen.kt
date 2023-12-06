b1f9f777b1edbd1441599e65940c2402db71175d
Paweł Osiński <pawelosinski000@gmail.com>
2023-12-07 00:32:27 +0100

    fix icon

--- app/src/main/kotlin/pl/pawelosinski/skatefreak/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/pl/pawelosinski/skatefreak/ui/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package pl.pawelosinski.skatefreak.ui.home
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,6 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,11 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import pl.pawelosinski.skatefreak.R
 import pl.pawelosinski.skatefreak.local.isDarkMode
 import pl.pawelosinski.skatefreak.ui.theme.SkateFreakTheme
 
@@ -42,10 +41,9 @@ fun HomeScreen(navController: NavController) {
                         .padding(horizontal = 15.dp, vertical = 10.dp)
                         .clip(MaterialTheme.shapes.large)
                 ) {
-                    Image(
-                        painter = painterResource(R.drawable.ic_launcher_foreground),
+                    Icon(
+                        imageVector = Icons.Default.Home,
                         contentDescription = "home_screen_bg",
-                        contentScale = ContentScale.Crop,
                         modifier = Modifier.fillMaxSize()
                     )
                 }
