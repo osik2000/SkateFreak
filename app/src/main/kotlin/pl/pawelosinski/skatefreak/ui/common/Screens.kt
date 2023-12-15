500baf88a89e517d0398d8724fadb353b62c0693
Paweł Osiński <pawelosinski000@gmail.com>
2023-12-16 00:30:19 +0100

    change class type

--- app/src/main/kotlin/pl/pawelosinski/skatefreak/ui/common/Screens.kt
+++ app/src/main/kotlin/pl/pawelosinski/skatefreak/ui/common/Screens.kt
@@ -1,13 +1,13 @@
 package pl.pawelosinski.skatefreak.ui.common
 
 sealed class Screens(val route : String) {
-    object Home : Screens("home")
-    object Tricks : Screens("tricks")
-    object AddRecord : Screens("addRecord")
-    object Profile : Screens("profile")
-    object Settings : Screens("settings")
-    object Login : Screens("login")
-    object TrickInfo : Screens("trickInfo/{trickId}") {
+    data object Home : Screens("home")
+    data object Tricks : Screens("tricks")
+    data object AddRecord : Screens("addRecord")
+    data object Profile : Screens("profile")
+    data object Settings : Screens("settings")
+    data object Login : Screens("login")
+    data object TrickInfo : Screens("trickInfo/{trickId}") {
         fun createRoute(trickId: String) = "trickInfo/$trickId"
     }
 
