da1849c06de16a04825442fed8dcc481c9e5917c
Paweł Osiński <308191@stud.umk.pl>
2023-12-23 00:32:30 +0100

    just pickerdraft to videoTrim

--- app/src/main/kotlin/pl/pawelosinski/skatefreak/ui/tricks/record/add/VideoPicker.kt
+++ app/src/main/kotlin/pl/pawelosinski/skatefreak/ui/tricks/record/add/VideoPicker.kt
@@ -1,6 +1,8 @@
 package pl.pawelosinski.skatefreak.ui.tricks.record.add
 
+import android.content.Context
 import android.net.Uri
+import android.provider.OpenableColumns
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -8,6 +10,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import pl.pawelosinski.skatefreak.model.TrickRecord
 import pl.pawelosinski.skatefreak.ui.common.myToast
 
 @Composable
@@ -21,8 +24,11 @@ fun VideoPickerButton() {
             myToast(context, "Nie wybrano pliku")
         }
         else {
-            myToast(context, "Wybrano plik: ${uri.lastPathSegment}")
-            Log.d("VideoPicker", "file chosen: ${uri.lastPathSegment}")
+            val fileName = getFileNameFromUri(context, uri)
+            myToast(context, "Wybrano plik: $fileName")
+            Log.d("VideoPicker", "file chosen: $fileName")
+            // TODO Implementation
+            TrickRecord.localFileUri.value = uri.toString()
 
         }
     }
@@ -30,7 +36,26 @@ fun VideoPickerButton() {
     Button(onClick = {
         pickMediaActivityResultLauncher.launch("video/*")
     }) {
-        Text(text = "Pick media content")
+        Text(text = "Wybierz nagranie")
     }
 
+}
+fun getFileNameFromUri(context: Context, uri: Uri): String {
+    var result = "Unknown"
+    try {
+        context.contentResolver.query(uri, null, null, null, null)?.use { cursor ->
+            if (cursor.moveToFirst()) {
+                val displayNameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (displayNameIndex >= 0) {
+                    val displayName = cursor.getString(displayNameIndex)
+                    result = displayName ?: "Unknown"
+                } else {
+                    Log.e("getFileNameFromUri", "Column DISPLAY_NAME not found in the cursor")
+                }
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return result
 }
